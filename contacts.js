390e227056c17f5c6331b5121c9fa5f51b19c3d4
Andriy323 <108237550+Andriy323@users.noreply.github.com>
2023-03-25 17:03:24 +0200

    add prettier

--- contacts.js
+++ contacts.js
@@ -1,46 +1,42 @@
-const fs = require("fs/promises");
-const path = require("path");
-const {nanoid} = require("nanoid");
+const fs = require('fs/promises');
+const path = require('path');
+const { nanoid } = require('nanoid');
 
-const contactsPath = path.join(__dirname, "db/contacts.json");
+const contactsPath = path.join(__dirname, 'db/contacts.json');
 
 const listContacts = async () => {
- 
-  const data = await fs.readFile(contactsPath, "utf-8");
+  const data = await fs.readFile(contactsPath, 'utf-8');
   return JSON.parse(data);
 };
 
-const getContactById = async (contactId) => {
- 
+const getContactById = async contactId => {
   const contact = await listContacts();
-  const resultContact = contact.find((item) => item.id === contactId);
+  const resultContact = contact.find(item => item.id === contactId);
   return resultContact || null;
 };
 
-const removeContact = async (contactId) => {
- 
+const removeContact = async contactId => {
   const contact = await listContacts();
-const id  = contact.findIndex(item => item.id === contactId)
-if(id === -1) {
-  return
-}
-const [result] = contact.splice(id, 1)
-await fs.writeFile(contactsPath, JSON.stringify(contact, null, 2))
-return result
+  const id = contact.findIndex(item => item.id === contactId);
+  if (id === -1) {
+    return;
+  }
+  const [result] = contact.splice(id, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contact, null, 2));
+  return result;
 };
 
 const addContact = async (name, email, phone) => {
-
   const contact = await listContacts();
   const newContact = {
     id: nanoid(),
     name,
     email,
-    phone
-  }
-contact.push(newContact)
-await fs.writeFile(contactsPath, JSON.stringify(contact, null, 2))
-return newContact
+    phone,
+  };
+  contact.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contact, null, 2));
+  return newContact;
 };
 
 module.exports = {
